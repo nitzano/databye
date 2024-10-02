618d85f3674cdba2843b8a83bdfa63f06aca21de
Nitzan Ohana <16689354+nitzano@users.noreply.github.com>
2024-10-02 16:37:24 +0300

    new lock

--- packages/datazar/src/databye.ts
+++ packages/datazar/src/databye.ts
@@ -5,7 +5,7 @@ import { Command } from "commander";
 import process from "node:process";
 // Import {anonDbCommand} from './commands/anon-db/anon-db-command';
 
-const program = new Command().alias("dz");
+const program = new Command().alias("dbye");
 
 program.addCommand(anonColCommand);
 program.parse(process.argv);
