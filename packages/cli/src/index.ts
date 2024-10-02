629e1d320a84411c871ca7b2f83cc426148a17b7
Nitzan Ohana <16689354+nitzano@users.noreply.github.com>
2024-10-02 10:56:39 +0300

    2 allow to run cli in prod (#9)
    * prod
    
    * build
    
    * deps
    
    * dec
    
    * knex
    
    * buikd
    
    * fix
    
    * datazar does not export anything
    
    * only soure maps
    
    * deploy out
    
    * template
    
    * template
    
    * datazar
    
    * datazar
    
    * add all
    
    * cli
    
    * help
    
    * add cli
    
    * seed
    
    * deps
    
    * set
    
    * deploy
    
    * not
    
    * add
    
    * rm
    
    * remove
    
    * remove
    
    * deploy
    
    * deps
    
    * return env
    
    * remove unused
    
    * second coomand
    
    * keep
    
    * change tsx to dev
    
    * pkg is dev
    
    * remove private flag

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,1 +1,5 @@
-export const hello = 'world';
+#!/usr/bin/env node
+
+import { Command } from "commander";
+export const program = new Command().alias("dz");
+export * from "./commands/anon-col/anon-col-command.js";
---
+++ packages/datazar/src/datazar.ts
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+import { anonColCommand } from "@datazar/cli";
+import { Command } from "commander";
+import process from "node:process";
+// Import {anonDbCommand} from './commands/anon-db/anon-db-command';
+
+const program = new Command().alias("dz");
+
+program.addCommand(anonColCommand);
+program.parse(process.argv);
