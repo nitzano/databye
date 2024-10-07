2288699f310defd27423c9e735a13020d493e2aa
Nitzan Ohana <16689354+nitzano@users.noreply.github.com>
2024-10-07 09:24:43 +0300

    exclude

--- packages/common/src/types/engine-type.ts
+++ packages/common/src/types/engine-type.ts
@@ -1,7 +1,8 @@
 export enum EngineType {
-	PostGres = 'postgres',
-	Mongo = 'mongo',
-	MariaDB = 'mariadb',
-	MySQL = 'mysql',
-	MSSQL = 'mssql',
+  PostGres = "postgres",
+  Mongo = "mongo",
+  MariaDB = "mariadb",
+  MySQL = "mysql",
+  MSSQL = "mssql",
+  SQLite = "sqlite",
 }
