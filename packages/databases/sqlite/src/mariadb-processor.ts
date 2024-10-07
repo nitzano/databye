bb2889745748636d49c84c7ee1f12d864207497f
Nitzan Ohana <16689354+nitzano@users.noreply.github.com>
2024-10-07 08:07:48 +0300

    new package

---
+++ packages/databases/sqlite/src/mariadb-processor.ts
@@ -0,0 +1,21 @@
+import { KnexProcessor } from "@databye/knex-processor";
+import knex, { type Knex } from "knex";
+
+export class MariaDatabaseProcessor extends KnexProcessor {
+  /**
+   * Build the mariadb client
+   *
+   * @return {*}  {Knex}
+   * @memberof MariaDatabaseProcessor
+   */
+  buildClient(): Knex {
+    if (!this.connectionOptions.connectionString) {
+      throw new Error("connection string is required");
+    }
+
+    return knex({
+      client: "mysql",
+      connection: this.connectionOptions.connectionString,
+    });
+  }
+}
