b5f61fb38b9fdb5d09bcc9133b974d943f32dcf1
Nitzan Ohana <16689354+nitzano@users.noreply.github.com>
2024-10-09 14:01:26 +0300

    move

--- packages/cli/src/anoymizers/mask/mask-action.ts
+++ packages/cli/src/anoymizers/mask/mask-action.ts
@@ -6,8 +6,8 @@ import {
 } from "@databye/anonymizers";
 import { createLogger } from "@databye/common";
 import { type Command } from "commander";
-import { extractConnectionOptions } from "../../helpers/extract-connection-options.js";
-import { processColumnHelper } from "../../helpers/process-column-helper.js";
+import { extractConnectionOptions } from "../../commands/anon-col/helpers/extract-connection-options.js";
+import { processColumnHelper } from "../../commands/anon-col/helpers/process-column-helper.js";
 
 const logger = createLogger();
 
