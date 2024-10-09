1f390ac2238a7bf6dbb18690f019352540a9087c
Nitzan Ohana <16689354+nitzano@users.noreply.github.com>
2024-10-09 14:42:16 +0300

    remove engine from connection options

--- packages/cli/src/anoymizers/scramble/scramble-command.ts
+++ packages/cli/src/anoymizers/scramble/scramble-command.ts
@@ -1,6 +1,6 @@
-import {Command} from 'commander';
-import {scrambleAction} from './scramble-action.js';
+import { Command } from "commander";
+import { scrambleAction } from "./scramble-action.js";
 
-export const scrambleCommand = new Command('scramble');
+export const scrambleCommand = new Command("scramble");
 
-scrambleCommand.description('scramble a single column').action(scrambleAction);
+scrambleCommand.description("scramble a single column").action(scrambleAction);
--- packages/common/src/types/connection-options.ts
+++ packages/common/src/types/connection-options.ts
@@ -1,11 +1,8 @@
-import { type EngineType } from "./engine-type.js";
-
 // @todo: rename to DatabaseOptions
 export type ConnectionOptions = {
   columnName: string;
   connectionString?: string;
   databaseName: string;
-  engine?: EngineType;
   filePath?: string;
   password?: string;
   serverName?: string;
