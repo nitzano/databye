d42a9399fb24734deddb3f2ba85251820e789dd2
Nitzan Ohana <16689354+nitzano@users.noreply.github.com>
2024-10-09 16:22:52 +0300

    shorter

--- packages/cli/src/anoymizers/mask/mask-action.ts
+++ packages/cli/src/anoymizers/mask/mask-action.ts
@@ -1,6 +1,11 @@
-import { type MaskOptions } from "@databye/anonymizers";
+import {
+  createMaskAnonymizer,
+  MaskAnonymizer,
+  type MaskOptions,
+} from "@databye/anonymizers";
 import { createLogger, EngineType } from "@databye/common";
 import { type Command } from "commander";
+import { ColumnProcessorRunner } from "../../anon-col/column-processor-runner.js";
 import { createProcessor } from "../../anon-col/helpers/create-processor.js";
 
 const logger = createLogger();
@@ -10,6 +15,9 @@ export async function maskAction(this: Command) {
     character: this.opts().character as string,
   };
 
+  // // Build anonymizer
+  const maskAnonymizer: MaskAnonymizer = createMaskAnonymizer(maskOptions);
+
   logger.debug(` ${JSON.stringify(maskOptions, null, 2)}`);
   const engineCommand = this.parent;
   if (engineCommand) {
@@ -18,21 +26,9 @@ export async function maskAction(this: Command) {
     logger.debug(`engineType = ${engineType}`);
     const engineOptions = engineCommand?.optsWithGlobals();
     const columnProcessor = createProcessor(engineType, engineOptions);
+    const runner = new ColumnProcessorRunner(columnProcessor, maskAnonymizer);
+    const isConfirmed = this.optsWithGlobals().confirm as boolean;
+    const columnName = this.optsWithGlobals().column as string;
+    await runner.processColumn(columnName, isConfirmed);
   }
-
-  // extract processor
-
-  // const connectionOptions = extractConnectionOptions(this);
-
-  // // Build anonymizer
-  // const maskAnonymizer: MaskAnonymizer = createMaskAnonymizer(maskOptions);
-
-  // const anonymizer: Anonymizer = maskAnonymizer;
-  // const isConfirmed = this.optsWithGlobals().confirm as boolean;
-  // logger.debug(`isConfined CLI = ${isConfirmed}`);
-
-  // // Anonymize column
-  // const runner = new ColumnProcessorRunner(null, anonymizer);
-  // const columnName = "col1";
-  // await runner.processColumn(columnName, isConfirmed);
 }
