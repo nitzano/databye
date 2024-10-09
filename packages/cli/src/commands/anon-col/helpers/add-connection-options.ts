76a2d7dc542027bc8f28f1bd267b9c0a0d7d38fc
Nitzan Ohana <16689354+nitzano@users.noreply.github.com>
2024-10-09 13:58:24 +0300

    remove

--- packages/cli/src/commands/anon-col/helpers/add-connection-options.ts
+++ packages/cli/src/commands/anon-col/helpers/add-connection-options.ts
@@ -2,11 +2,10 @@ import { type Command } from "commander";
 
 export function addConnectionOptions(cmd: Command): Command {
   return cmd
-    .requiredOption("-col --column <columnName>", "Column name")
     .option("-t --table <tableName>", "Table name")
     .option("-db --database <databaseName>", "Database name")
     .option("-pass --password <password>", "database password")
     .option("-srv --server <serverName>", "server to connect to")
-    .option("-u --uri <connectionString>", "Connection string")    
+    .option("-u --uri <connectionString>", "Connection string")
     .option("-usr --user <userName>", "Username to use");
 }
