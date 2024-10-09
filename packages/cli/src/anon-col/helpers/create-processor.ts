94d0b5536067cdb8229d7b674a28dfe825fd1a59
Nitzan Ohana <16689354+nitzano@users.noreply.github.com>
2024-10-09 15:15:31 +0300

    rename

--- packages/cli/src/anon-col/helpers/create-processor.ts
+++ packages/cli/src/anon-col/helpers/create-processor.ts
@@ -8,7 +8,7 @@ import { MariaDatabaseProcessor } from "@databye/mariadb";
 import { MongoProcessor } from "@databye/mongo";
 import { MsSqlProcessor } from "@databye/mssql";
 import { PostgresProcessor } from "@databye/postgres";
-import { type DataBaseProcessor } from "@databye/processor";
+import { type ColumnProcessor } from "@databye/processor";
 import { SQLiteProcessor } from "@databye/sqlite";
 import { getProcessorEngineFromUri } from "../../utils/get-processor-engine-from-uri.js";
 
@@ -16,7 +16,7 @@ const logger = createLogger();
 
 export async function createProcessor(
   connectionOptions: ConnectionOptions
-): Promise<DataBaseProcessor | undefined> {
+): Promise<ColumnProcessor | undefined> {
   // Parse the engine
   const { connectionString, engine: connectionEngine } = connectionOptions;
   let engine: EngineType | undefined;
@@ -31,38 +31,38 @@ export async function createProcessor(
   }
 
   logger.debug(`engine = ${engine}`);
-  let databaseProcessor: DataBaseProcessor | undefined;
+  let columnProcessor: ColumnProcessor | undefined;
   if (engine) {
     switch (engine) {
       case EngineType.Mongo: {
         logger.debug("creating mongo processor");
         if (!connectionString) throw new Error("invalid connection string uri");
-        databaseProcessor = new MongoProcessor(connectionString);
+        columnProcessor = new MongoProcessor(connectionString);
         break;
       }
 
       case EngineType.PostGres: {
         logger.debug("creating postgress processor");
-        databaseProcessor = new PostgresProcessor(connectionOptions);
+        columnProcessor = new PostgresProcessor(connectionOptions);
         break;
       }
 
       case EngineType.MariaDB:
       case EngineType.MySQL: {
         logger.debug("creating mariadb processor");
-        databaseProcessor = new MariaDatabaseProcessor(connectionOptions);
+        columnProcessor = new MariaDatabaseProcessor(connectionOptions);
         break;
       }
 
       case EngineType.MSSQL: {
         logger.debug("creating mssql processor");
-        databaseProcessor = new MsSqlProcessor(connectionOptions);
+        columnProcessor = new MsSqlProcessor(connectionOptions);
         break;
       }
 
       case EngineType.SQLite: {
         logger.debug("creating sqlite processor");
-        databaseProcessor = new SQLiteProcessor(connectionOptions);
+        columnProcessor = new SQLiteProcessor(connectionOptions);
         break;
       }
 
@@ -71,11 +71,11 @@ export async function createProcessor(
         if (!connectionOptions.filePath) {
           throw new Error("invalid file path");
         }
-        databaseProcessor = new CSVProcessor(connectionOptions.filePath);
+        columnProcessor = new CSVProcessor(connectionOptions.filePath);
         break;
       }
     }
   }
 
-  return databaseProcessor;
+  return columnProcessor;
 }
