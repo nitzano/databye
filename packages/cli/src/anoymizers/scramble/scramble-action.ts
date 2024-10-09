f745d3e56a69244de0eb901f02ca51a8f28754cc
Nitzan Ohana <16689354+nitzano@users.noreply.github.com>
2024-10-09 16:52:01 +0300

    remove this

--- packages/cli/src/anoymizers/scramble/scramble-action.ts
+++ packages/cli/src/anoymizers/scramble/scramble-action.ts
@@ -6,8 +6,6 @@ import { type Command } from "commander";
 import { runAnonymizerCommand } from "../run-anonymizer-command.js";
 
 export async function scrambleAction(this: Command) {
-  // const connectionOptions = extractConnectionOptions(this);
-
   // // Build anonymizer
   const scrambleAnonymizer: ScrambleAnonymizer = createScrambleAnonymizer();
   await runAnonymizerCommand(this.parent!, scrambleAnonymizer);
