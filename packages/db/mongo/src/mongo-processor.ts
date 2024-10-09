95876c85e43f5178c7a1050fd7689bb86eb53c49
Nitzan Ohana <16689354+nitzano@users.noreply.github.com>
2024-10-09 15:01:41 +0300

    fix

--- packages/db/mongo/src/mongo-processor.ts
+++ packages/db/mongo/src/mongo-processor.ts
@@ -1,6 +1,6 @@
 import { ColumnType, type Anonymizer } from "@databye/anonymizers";
 import { createLogger } from "@databye/common";
-import { DataBaseProcessor, type ColumnInfo } from "@databye/processor";
+import { DataBaseProcessor } from "@databye/processor";
 import { MongoClient, type Db } from "mongodb";
 
 const logger = createLogger();
@@ -38,22 +38,21 @@ export class MongoProcessor extends DataBaseProcessor {
   }
 
   async processColumn(
-    columnInfo: ColumnInfo,
+    columnName: string,
     columnType: ColumnType,
     anonymizer: Anonymizer
   ) {
-    const { databaseName, tableName, columnName } = columnInfo;
     logger.debug(`mongo:processColumn ${columnName}`);
     const client = new MongoClient(this.uri);
     let database: Db;
     try {
       await client.connect();
-      database = client.db(databaseName);
+      database = client.db(this.databaseName);
 
       // Process column
       await this.processCollection(
         database,
-        tableName,
+        this.tableName,
         columnName,
         columnType,
         anonymizer
