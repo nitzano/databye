b95a065dd4973d358d455727c95573fdb47a5fbf
Nitzan Ohana <16689354+nitzano@users.noreply.github.com>
2024-10-09 16:29:19 +0300

    set

--- packages/cli/src/anon-col/helpers/check-user-confirm.ts
+++ packages/cli/src/anon-col/helpers/check-user-confirm.ts
@@ -6,26 +6,16 @@ const { prompt } = Enquirer;
 // Cyan
 const color = chalk.cyan;
 
-export async function checkUserConfirm(
-  column: string,
-  database?: string,
-  table?: string
-): Promise<boolean> {
+export async function checkUserConfirm(column: string): Promise<boolean> {
   try {
     let confirmMessage = `Are you sure you want to anonymize column "${color(
       column
     )}"`;
 
-    if (database && table) {
-      confirmMessage += ` in "${color(database)}/${color(table)}"`;
-    }
-
     const answer = await prompt<{ run: boolean }>({
       type: "confirm",
       name: "run",
-      message: `Are you sure you want to anonymize column "${color(
-        column
-      )}" in "${color(database)}/${color(table)}" `,
+      message: confirmMessage,
       initial: "true",
     });
     if (answer.run) {
