ef6f52d7df15573d0047a8306b7c6d056717cf77
Nitzan Ohana <16689354+nitzano@users.noreply.github.com>
2024-10-10 11:38:29 +0300

    remove unused

--- packages/databye/src/databye.ts
+++ packages/databye/src/databye.ts
@@ -3,7 +3,6 @@
 import { anonColCommand } from "@databye/cli";
 import { Command } from "commander";
 import process from "node:process";
-// Import {anonDbCommand} from './commands/anon-db/anon-db-command';
 
 const program = new Command().alias("dbye");
 
