5017b745e1cb05b6a9930dd2d6fc676ed934e0fe
Nitzan Ohana <16689354+nitzano@users.noreply.github.com>
2024-10-11 10:41:57 +0300

    mutable

--- packages/cli/src/anon-col/helpers/add-file-options.ts
+++ packages/cli/src/anon-col/helpers/add-file-options.ts
@@ -1,5 +1,5 @@
 import { type Command } from "commander";
 
-export function addFileOptions(cmd: Command): Command {
-  return cmd.requiredOption("-f --file <filePath>", "File path");
+export function addFileOptions(cmd: Command) {
+  cmd.requiredOption("-f --file <filePath>", "File path");
 }
