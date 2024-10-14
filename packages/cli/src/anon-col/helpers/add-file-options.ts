8c03884f314b786866236e3f9454d1e2378ceb18
Nitzan Ohana <16689354+nitzano@users.noreply.github.com>
2024-10-14 15:01:43 +0300

    remove shortcut

--- packages/cli/src/anon-col/helpers/add-file-options.ts
+++ packages/cli/src/anon-col/helpers/add-file-options.ts
@@ -1,5 +1,5 @@
 import { type Command } from "commander";
 
 export function addFileOptions(cmd: Command) {
-  cmd.requiredOption("-f --file <filePath>", "File path");
+  cmd.requiredOption("--file <filePath>", "File path");
 }
