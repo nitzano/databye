a7f66e625d8af9d9a55ad009615b17a1462f5e99
Nitzan Ohana <16689354+nitzano@users.noreply.github.com>
2024-10-14 16:58:19 +0300

    add mask

--- packages/cli/src/anoymizers/mask/mask-command.ts
+++ packages/cli/src/anoymizers/mask/mask-command.ts
@@ -2,13 +2,13 @@ import { defaultMaskOptions } from "@databye/anonymizers";
 import { Command } from "commander";
 import { maskAction } from "./mask-action.js";
 
-export const maskCommand = new Command("mask");
-
-maskCommand
-  .description("mask a single column")
-  .option(
-    "-c --character <char>",
-    "Character to mask with",
-    defaultMaskOptions.character
-  )
-  .action(maskAction);
+export function getMaskCommand(): Command {
+  return new Command("mask")
+    .description("mask a single column")
+    .option(
+      "-c --character <char>",
+      "Character to mask with",
+      defaultMaskOptions.character
+    )
+    .action(maskAction);
+}
