6d4554340cc801e0fe526aee6ab9d0f70e2c18bd
Nitzan Ohana <16689354+nitzano@users.noreply.github.com>
2024-10-14 16:44:03 +0300

    add to db

--- packages/cli/src/anon-col/helpers/add-column-option.ts
+++ packages/cli/src/anon-col/helpers/add-column-option.ts
@@ -1,5 +1,5 @@
 import { Command } from "commander";
 
 export function addColumnOption(cmd: Command) {
-  cmd.requiredOption("--column <columnName>", "Column name to process");
+  cmd.requiredOption("-col --column <columnName>", "Column name to process");
 }
--- packages/cli/src/anon-col/helpers/add-connection-options.ts
+++ packages/cli/src/anon-col/helpers/add-connection-options.ts
@@ -3,7 +3,7 @@ import { type Command } from "commander";
 export function addConnectionOptions(cmd: Command): Command {
   return cmd
     .option("--table <tableName>", "Table name")
-    .option("--database <databaseName>", "Database name")
+    .option("-db --database <databaseName>", "Database name")
     .option("--password <password>", "database password")
     .option("--server <serverName>", "server to connect to")
     .option("--uri <connectionString>", "Connection string")
