0e14a800064bd7eccd2c74129918bc35f2726c35
Nitzan Ohana <16689354+nitzano@users.noreply.github.com>
2024-10-15 16:17:14 +0300

    remove duplicates

--- packages/common/src/index.ts
+++ packages/common/src/index.ts
@@ -1,5 +1,5 @@
-export * from './anoymizers/index.js';
+export * from './anonymizers/index.js';
 export * from './logger/index.js';
-export * from './processors/index.js';
 export * from './test/index.js';
 export * from './types/index.js';
+
