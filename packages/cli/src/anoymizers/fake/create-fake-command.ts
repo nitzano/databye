71e14e59a43ae5a994fd91b9a3cf17c527d56cf0
Nitzan Ohana <16689354+nitzano@users.noreply.github.com>
2024-10-15 15:47:52 +0300

    set

--- packages/cli/src/anoymizers/fake/create-fake-command.ts
+++ packages/cli/src/anoymizers/fake/create-fake-command.ts
@@ -1,10 +1,11 @@
 import { FakeSource } from "@databye/anonymizers";
 import { Command, Option } from "commander";
+import { fakeAction } from "./fake-action.js";
 
 export function createFakeCommand(): Command {
     return new Command("fake")
         .description("Fill with a fake value")
         .addOption(
             new Option('--source --provider <source>', 'Fake Provider').default(FakeSource.Word).choices(Object.values(FakeSource))
-        )
+        ).action(fakeAction)
 }
--- packages/cli/src/anoymizers/scramble/create-scramble-command.ts
+++ packages/cli/src/anoymizers/scramble/create-scramble-command.ts
@@ -5,4 +5,5 @@ export function createScrambleCommand(): Command {
   return new Command("scramble")
     .description("scramble a single column")
     .action(scrambleAction);
+
 }
