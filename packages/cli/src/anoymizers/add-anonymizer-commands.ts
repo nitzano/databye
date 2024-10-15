33fcacf5d316e01f02ac9ccbd7595b04bca67f0b
Nitzan Ohana <16689354+nitzano@users.noreply.github.com>
2024-10-15 15:29:47 +0300

    set

--- packages/cli/src/anoymizers/add-anonymizer-commands.ts
+++ packages/cli/src/anoymizers/add-anonymizer-commands.ts
@@ -1,10 +1,10 @@
 import { Command } from "commander";
+import { createFakeCommand } from "./fake/create-fake-command.js";
 import { createMaskCommand } from "./mask/create-mask-command.js";
 import { createScrambleCommand } from "./scramble/create-scramble-command.js";
 
 export function addAnonymizerCommands(cmd: Command) {
-  const scrambleCommand = createScrambleCommand();
-  const maskCommand = createMaskCommand();
-  cmd.addCommand(scrambleCommand);
-  cmd.addCommand(maskCommand);
+  cmd.addCommand(createScrambleCommand());
+  cmd.addCommand(createMaskCommand());
+  cmd.addCommand(createFakeCommand())
 }
