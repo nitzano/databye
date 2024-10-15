48755743edefb51e98b2fa02744f2513e2fd8058
Nitzan Ohana <16689354+nitzano@users.noreply.github.com>
2024-10-15 12:29:45 +0300

    add enums

--- packages/anonymizers/src/providers/fake/types.ts
+++ packages/anonymizers/src/providers/fake/types.ts
@@ -1,23 +1,38 @@
+export enum FakeStringProviders {
+  FirstName = "first_name",
+  LastName = "last_name",
+  Word = "word",
+  Name = "name",
+  Animal = "animal",
+  Email = "email",
+}
+
+export enum FakeNumberProviders {
+  Age = "age",
+}
+
+// @deprecated
 export const fakeTypes: string[] = [
-	'age',
-	'email',
-	'first',
-	'firstName',
-	'last',
-	'lastName',
-	'letter',
-	'name',
-	'word',
+  "age",
+  "email",
+  "first",
+  "firstName",
+  "last",
+  "lastName",
+  "letter",
+  "name",
+  "word",
 ];
 
+// @deprecated
 export type FakeType =
-	| 'age'
-	| 'animal'
-	| 'email'
-	| 'first'
-	| 'firstName'
-	| 'last'
-	| 'lastName'
-	| 'letter'
-	| 'name'
-	| 'word';
+  | "age"
+  | "animal"
+  | "email"
+  | "first"
+  | "firstName"
+  | "last"
+  | "lastName"
+  | "letter"
+  | "name"
+  | "word";
