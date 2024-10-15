93ccd1f16f7c09ba83ee18696afa49ca7542e75f
Nitzan Ohana <16689354+nitzano@users.noreply.github.com>
2024-10-15 17:23:30 +0300

    cyan

--- packages/cli/src/anoymizers/run-anonymize-column-action.ts
+++ packages/cli/src/anoymizers/run-anonymize-column-action.ts
@@ -28,7 +28,7 @@ export async function runAnonymizeColumnAction(
   const isConfirmed = engineCommand.optsWithGlobals().confirm as boolean;
   const columnName = engineCommand.optsWithGlobals().column as string;
   // logger.info(`using ${chalk.cyan("🍀 fake")} anonymizer`)
-  logger.info(`Anonymizing ${chalk.green(columnName)} column with ${chalk.green(engineType)} engine`);
+  logger.info(`Anonymizing ${chalk.cyan(columnName)} column with ${chalk.cyan(engineType)} engine`);
   logger.info(`Using ${anonymizerToIcon[anonymizer.name]} ${chalk.magenta(anonymizer.name)} anonymizer`)
   await runner.processColumn(columnName, isConfirmed);
   logger.info('✅ Done!')
