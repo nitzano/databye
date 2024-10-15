16c5a5fe82010ce3e9e74999b4b0c0f7a386452e
Nitzan Ohana <16689354+nitzano@users.noreply.github.com>
2024-10-15 17:02:52 +0300

    better

--- packages/cli/src/anoymizers/run-anonymize-column-action.ts
+++ packages/cli/src/anoymizers/run-anonymize-column-action.ts
@@ -1,7 +1,10 @@
 import { Anonymizer, createLogger, EngineType } from "@databye/common";
+import chalk from "chalk";
 import { Command } from "commander";
 import { ColumnProcessorRunner } from "../anon-col/column-processor-runner.js";
 import { createProcessor } from "../anon-col/helpers/create-processor.js";
+
+const { cyanBright } = chalk
 
 const logger = createLogger();
 
@@ -15,6 +18,6 @@ export async function runAnonymizeColumnAction(
   const runner = new ColumnProcessorRunner(columnProcessor, anonymizer);
   const isConfirmed = engineCommand.optsWithGlobals().confirm as boolean;
   const columnName = engineCommand.optsWithGlobals().column as string;
-  logger.info(`Anonymizing column ${columnName} with engine ${engineType}`);
+  logger.info(`Anonymizing column ${cyanBright(columnName)} with ${cyanBright(engineType)} engine`);
   await runner.processColumn(columnName, isConfirmed);
 }
