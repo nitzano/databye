84e24caeb775255e336c54d1d4cf8d7cec731ddc
Nitzan Ohana <16689354+nitzano@users.noreply.github.com>
2024-10-15 17:10:53 +0300

    name

--- packages/common/src/anonymizers/anonymizer.ts
+++ packages/common/src/anonymizers/anonymizer.ts
@@ -1,6 +1,8 @@
 import { ColumnType } from "./column-type.js";
+import { ProviderType } from "./provider-type.js";
 
 export type Anonymizer = {
+    name: ProviderType,
     anonymizeString?: (value: string) => string;
     anonymizeNumber?: (value: number) => number;
     anonymizeBoolean?: (value: boolean) => boolean;
